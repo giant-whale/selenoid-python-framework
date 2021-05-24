e21221a4113729e75f728f2061619401b5f5f70d
Giant Whale <github@beautiful-electricity.com>
2021-05-24 17:34:18 +0300

    allure-reports-1 - exceptions

--- core/locator.py
+++ core/locator.py
@@ -1,6 +1,8 @@
 import allure
+from selenium.common.exceptions import InvalidSelectorException
 
 from core.driver import Driver
+from core.exceptions import CustomBrokenException
 from core.helpers import (
     wait_for_page_to_change,
     wait_for_page_to_load,
@@ -20,8 +22,11 @@ class Locator:
         self.name = name
 
     def __call__(self):
-        self.webelement = Driver().find_element_by_xpath(self.xpath)
-        return self
+        try:
+            self.webelement = Driver().find_element_by_xpath(self.xpath)
+            return self
+        except InvalidSelectorException:
+            raise CustomBrokenException(f'Error: cannot locate {self.name}')
 
     def click(self, wait_for_new_page: bool = False):
         with allure.step(f'Click on {self.name}'):
