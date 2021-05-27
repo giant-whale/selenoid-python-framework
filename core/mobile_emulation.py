3279b60426a269999ce870c68b3897ddf2d9a04b
Giant Whale <github@beautiful-electricity.com>
2021-05-27 19:40:14 +0300

    mobile-emulation-1 - new exception, pytest mark and mobile device

---
+++ core/mobile_emulation.py
@@ -0,0 +1,5 @@
+device_iphone8 = {
+    "deviceMetrics": {"width": 375, "height": 667, "pixelRatio": 2.0},
+    "userAgent": "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) "
+                 "AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1"
+}
