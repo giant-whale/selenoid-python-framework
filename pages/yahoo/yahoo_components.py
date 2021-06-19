e472bc4fbf6f0c846f3a4a856443514fdf0044a3
Giant Whale <github@beautiful-electricity.com>
2021-06-19 09:54:29 +0300

    cloudmore - remove yahoo

--- pages/yahoo/yahoo_components.py
+++
@@ -1,6 +0,0 @@
-from core.locator import Locator
-
-
-class YahooSearchBar:
-    search_input = Locator('Search Input', '//*[@role="search"]//input[@type="text"]')
-    search_submit = Locator('Search Submit', '//*[@role="search"]//input[@type="submit"]')
--- pages/yahoo/yahoo_main_page.py
+++
@@ -1,8 +0,0 @@
-from core.page import Page
-from pages.yahoo.yahoo_components import YahooSearchBar
-
-
-class YahooMainPage(Page):
-    BASE_PAGE_URL = "https://www.yahoo.com/"
-
-    YahooSearchBar = YahooSearchBar()
