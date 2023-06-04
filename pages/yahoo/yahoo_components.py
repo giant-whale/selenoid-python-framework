2710024cb1df64b3a2289f6a454e12ab969b7cbd
Nikolay Zakharov <offer@beautiful-electricity.com>
2023-06-04 20:19:37 +0300

    Newlines, update xpath locators

--- pages/yahoo/yahoo_components.py
+++ pages/yahoo/yahoo_components.py
@@ -3,4 +3,4 @@ from core.locator import Locator
 
 class YahooSearchBar:
     search_input = Locator('Search Input', '//*[@role="search"]//input[@type="text"]')
-    search_submit = Locator('Search Submit', '//*[@role="search"]//input[@type="submit"]')
+    search_submit = Locator('Search Submit', '//button[@id="ybar-search"]')
